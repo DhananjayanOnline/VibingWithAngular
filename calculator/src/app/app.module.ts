eba0b964075b0ab4bef913569a86c9bd45251097
Dhananjayan <dhananjayan.online@outlook.com>
2023-01-10 11:50:40 +0530

    addtion component created

--- calculator/src/app/app.module.ts
+++ calculator/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import { AdditionComponent } from './addition/addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MycomponentComponent
+    MycomponentComponent,
+    AdditionComponent
   ],
   imports: [
     BrowserModule,
