4624044f49d33a3633468cd182b0223bbe22ca00
Dhananjayan <dhananjayan.online@outlook.com>
2023-01-11 20:15:48 +0530

    factorial component function completed

--- calculator/src/app/prime-number/prime-number.component.ts
+++ calculator/src/app/prime-number/prime-number.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./prime-number.component.css']
 })
 export class PrimeNumberComponent {
+  num:number = 0
+  result:any = ""
+
+  prime(){
+    let flag:boolean = true
+    for(let i=2; i<this.num; i++){
+      if(this.num % i ===0){
+        flag = false
+        break
+      }
+    }
+
+    if(flag==true){
+      this.result = `${this.num} is a prime number`
+    }else{
+      this.result = `${this.num} is not a prime number`
+    }
+  }
+
+  
 
 }
