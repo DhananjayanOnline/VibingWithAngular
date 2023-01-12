d96f33bf3bec57a82dd29b0d1d19325fbf15405f
Dhananjayan <dhananjayan.online@outlook.com>
2023-01-12 11:04:21 +0530

    routing added

--- calculator/src/app/app-routing.module.ts
+++ calculator/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdditionComponent } from './addition/addition.component';
+import { FabinocciComponent } from './fabinocci/fabinocci.component';
+import { FactorialComponent } from './factorial/factorial.component';
+import { MultiplicationComponent } from './multiplication/multiplication.component';
+import { PrimeNumberComponent } from './prime-number/prime-number.component';
+import { SubtractionComponent } from './subtraction/subtraction.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"addition", component:AdditionComponent},
+  {path:"subtration", component:SubtractionComponent},
+  {path:"product", component:MultiplicationComponent},
+  {path:"factorial", component:FactorialComponent},
+  {path:"prime", component:PrimeNumberComponent},
+  {path:"fabinocci", component:FabinocciComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
