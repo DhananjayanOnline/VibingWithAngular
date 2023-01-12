a78936392d6f625965c8b11badfc8060e5d21842
Dhananjayan <dhananjayan.online@outlook.com>
2023-01-12 14:24:44 +0530

    sign component created

--- calculator/src/app/app-routing.module.ts
+++ calculator/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { FabinocciComponent } from './fabinocci/fabinocci.component';
 import { FactorialComponent } from './factorial/factorial.component';
 import { MultiplicationComponent } from './multiplication/multiplication.component';
 import { PrimeNumberComponent } from './prime-number/prime-number.component';
+import { SigninComponent } from './signin/signin.component';
 import { SubtractionComponent } from './subtraction/subtraction.component';
 
 
 const routes: Routes = [
+  {path:"login", component:SigninComponent},
   {path:"addition", component:AdditionComponent},
   {path:"subtraction", component:SubtractionComponent},
   {path:"product", component:MultiplicationComponent},
